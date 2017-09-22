7e11755b49e0f6a6de95e88028e1b5e4c0cff3b3
DreamTaleCore <dreamtalewind@gmail.com>
2017-09-22 12:15:39 +0800

    write a divide-and-conquer demo

--- src/algorithm.cpp
+++ src/algorithm.cpp
@@ -14,9 +14,28 @@ namespace test
 {
 	// Test function and modules here
 
+	// The complexity of this divide-and-conquer function is 
+	// O(lg n)
+	template<typename T>
+	T dncPower(T x, T n)
+	{
+		T rlt;
+		// bounder condition
+		if (n == 1) return x;
+		// divide problem into sub-problems recursively
+		if (n % 2 == 0) rlt = dncPower(x, n / 2) * dncPower(x, n / 2);
+		else rlt = dncPower(x, (n + 1) / 2) * dncPower(x, (n - 1) / 2);
+		
+		return rlt;
+	}
+
 }
 
 int main()
 {
 	// Main algorithm structure here
+	int x = 5;
+	int n = 3;
+	cout << "Power(" << x << "," << n << ") = " << test::dncPower<int>(x, n) << endl;
+	getchar();
 }
